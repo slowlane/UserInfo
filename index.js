a316f96ee90da3ce4004c862a734a02fc64de95b
Andreas <andreas.wickerfalt@gmail.com>
2023-06-15 13:34:21 +0200

    Creating li for each user

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const userDiv = document.getElementById('users');
+const userList = document.getElementById('user-list');
+
 
 const users = [];
 fetch('https://reqres.in/api/users/')
@@ -12,7 +14,13 @@ fetch('https://reqres.in/api/users/')
     for(i = 0; i < data.data.length; i++){
       users.push(data.data[i]);
     }
+
+    users.map((user) => {
+      console.log(user);
+      let li = document.createElement('li');
+      li.classList.add('person');
+      li.textContent = user.id;
+      userList.appendChild(li)
+    })
   })
   .catch(error => console.error('Det har uppstått ett probled vid hämtning av data: ', error));
-
-console.log(users);
