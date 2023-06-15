32a5b3d2b6fec0eded074bbb1a94d9c194bde339
Andreas <andreas.wickerfalt@gmail.com>
2023-06-15 14:01:01 +0200

    Added names to the page

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const userList = document.getElementById('user-list');
 
 
 const users = [];
+
+
 fetch('https://reqres.in/api/users/')
   .then(res => {
     if (!res.ok) {
@@ -11,23 +13,45 @@ fetch('https://reqres.in/api/users/')
     return res.json();
   })
   .then(data => {
+    initializePage(data);
+  })
+  .catch(error => console.error('Det har uppstått ett problem vid hämtning av data: ', error));
+
+
+  userList.addEventListener('click', (e) => {
+    console.log(e.target);
+  })
+
+
+
+function renderUsers(){
+  users.map((user) => {
+    let img = document.createElement('img');
+    let paragraph = document.createElement('p');
+    let li = document.createElement('li');
+    
+    img.src=`${user.avatar}`;
+    li.appendChild(img);
+    li.classList.add('person');
+
+    paragraph.innerText = user.first_name;
+    li.appendChild(paragraph);
+    
+    console.log(user);
+
+
+    userList.appendChild(li);
+
+  })
+}
+
+
+  function initializePage(data){
     for(i = 0; i < data.data.length; i++){
       users.push(data.data[i]);
     }
 
-    users.map((user) => {
-      let img = document.createElement('img');
-      let li = document.createElement('li');
-      
-      img.src=`${user.avatar}`;
-      li.appendChild(img);
-      li.classList.add('person');
-      
-      console.log(user);
+    renderUsers();
 
 
-      userList.appendChild(li);
-
-    })
-  })
-  .catch(error => console.error('Det har uppstått ett probled vid hämtning av data: ', error));
+  }
